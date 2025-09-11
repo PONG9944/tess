3ba384190fcd7241df751eeae3bb40eab2e1e527
Pong9944 <70256658+PONG9944@users.noreply.github.com>
2025-09-11 21:49:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import uvicorn
 # The variable name must be `app` for Render to automatically detect it.
 app = FastAPI()
 
-@app.post("/endpoint")
-def read_root(Va):
-    return {"message": Va.data[0] * 10}
-
-
 
 
 
